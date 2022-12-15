8868e0ea6535c8810117a4683219024629f22b6f
Irina <irina-rozhkova-87@mail.ru>
2022-12-15 10:41:58 +0300

    zadachi

--- proekt/Program.cs
+++ proekt/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Задача: 
+// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+
+
+/*Console.Write("Введите массив данных через ',': ");
+string strLine = Console.ReadLine();
+string[] inArr = strLine.Split(",");
+string[] outArr = new string[0];
+for (int i = 0; i < inArr.Length; i++)
+{
+    if (inArr[i].Length <= 3)
+    {
+        Array.Resize(ref outArr, outArr.Length + 1);
+        outArr[outArr.Length-1] = inArr[i];
+    }
+}
+Console.WriteLine($"[{string.Join(",", inArr)}]=>[{string.Join(",", outArr)}]");
+*/
+
+
